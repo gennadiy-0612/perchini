778d9e5b0090f742ae0178b147ddf963d79f2f4a
Genn <intval@i.ua>
2021-05-29 17:31:01 +0300

    Start

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-module.exports = {
-    plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-    ]
-}
+import './index.css'
 Vue.config.productionTip = false
 
 new Vue({
