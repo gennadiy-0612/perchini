fdfa227d54282ad74df86094a76927687d0ab573
Genn <intval@i.ua>
2021-05-29 13:19:38 +0300

    Start

---
+++ src/Photo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Photo" src="./assets/img/Photo.svg">
+    <Photo msg="Photo"/>
+  </div>
+</template>
+
+<script>
+import Photo from './components/Photo.vue'
+
+export default {
+  name: 'Photo',
+  components: {
+    Photo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
